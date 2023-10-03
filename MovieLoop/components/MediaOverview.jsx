a17be14eb1f833fe0234d1f20d88781361ebd203
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-03 19:18:27 -0300

    fix available at on details pages

--- MovieLoop/components/MediaOverview.jsx
+++ MovieLoop/components/MediaOverview.jsx
@@ -1,26 +1,21 @@
 import { render } from "react-dom"
 import { renderCast } from "../utils"
 import placeHolder from '../src/assets/images/poster_placeholder.jpg'
+
 export default function Overview({ media }) {
-  const directors = media.credits?.crew.filter(({ job }) => job === 'Director')
+  const directors = media.credits?.crew
+    .filter(({ job }) => job === 'Director')
     .map((director) => director.name)
+
   const creators = media.created_by?.map((creator) => creator.name) || ''
 
-  const genres = media.genres?.map((genre, index) => {
-    return genre.name
-  })
+  const genres = media.genres?.map((genre) => genre.name)
 
-  const companies = media.production_companies?.map((companie, index) => {
+  const companies = media.production_companies?.map((companie) => {
     return companie.name.charAt(0).toUpperCase() + companie.name.slice(1)
   })
 
   const cast = renderCast(media.credits?.cast, 'Cast')
-
-  const date = new Date(media.release_date || media.first_air_date).toLocaleDateString('en-gb', {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  })
 
   function toLocaleDateMedia(media) {
     return new Date(media).toLocaleDateString('en-gb', {
@@ -39,6 +34,7 @@ export default function Overview({ media }) {
 
   const detailItems = [
     { title: media.original_title ? 'Released' : 'First Aired', content: toLocaleDateMedia(media.release_date || media.first_air_date) },
+    { title: 'Released', content: 'hoje' },
     { title: 'Last Aired', content: media.last_air_date ? toLocaleDateMedia(media.last_air_date) : '' },
     { title: 'Runtime', content: media.original_title ? media.runtime + ' minutes' : '' },
     { title: media.created_by?.length >= 1 ? 'Creator' : directors?.length >= 1 && 'Director', content: creators || directors },
@@ -56,6 +52,7 @@ export default function Overview({ media }) {
     if (item.content) {
       return (
         <li key={`${item.title}-${index}`}>
+          {console.log(item.title)}
           <span className="detail-li-title">{item.title}</span>
           <span className="detail-li-content">{item.content}</span>
         </li>
@@ -64,26 +61,43 @@ export default function Overview({ media }) {
     return null
   })
 
-  let usProviders = ''
-  let renderAvailableAt = ''
-  if (media['watch/providers']) {
-    if ('US' in (media['watch/providers']?.results)) {
-      if ('buy' in (media['watch/providers'].results.US)) {
-        usProviders = media['watch/providers'].results.US.buy
-      } else if ('flatrate' in (media['watch/providers'].results.US)) {
-        usProviders = media['watch/providers'].results.US.flatrate
-      }
-      renderAvailableAt = usProviders.map((provider, index) => {
-        return (
-          <>
-            <img key={`provider-${index}`} src={`https://image.tmdb.org/t/p/original/${provider.logo_path}`}
-              className="company-logo"
-            />
-          </>
-        )
-      })
-    }
+  let usProviders = []
+  let renderAvailableAt = []
+
+
+  if (media['watch/providers'].results.US) {
+    media['watch/providers'].results.US?.buy ? usProviders = [...media['watch/providers'].results.US.buy] : usProviders
+    media['watch/providers'].results.US?.rent ? usProviders = [...media['watch/providers'].results.US.rent] : usProviders
+    usProviders = [...media['watch/providers'].results.US?.flatrate] || usProviders
+    renderAvailableAt = usProviders.map((provider, index) => {
+      return (
+        <>
+          <img key={`provider-${index}`} src={`https://image.tmdb.org/t/p/original/${provider.logo_path}`}
+            className="company-logo"
+          />
+        </>
+      )
+    })
   }
+
+  // if (media['watch/providers']) {
+  //   if ('US' in (media['watch/providers']?.results)) {
+  //     if ('buy' in (media['watch/providers'].results.US)) {
+  //       usProviders = media['watch/providers'].results.US.buy
+  //     } else if ('flatrate' in (media['watch/providers'].results.US)) {
+  //       usProviders = media['watch/providers'].results.US.flatrate
+  //     }
+  //     renderAvailableAt = usProviders.map((provider, index) => {
+  //       return (
+  //         <>
+  //           <img key={`provider-${index}`} src={`https://image.tmdb.org/t/p/original/${provider.logo_path}`}
+  //             className="company-logo"
+  //           />
+  //         </>
+  //       )
+  //     })
+  //   }
+  // }
 
   return (
     <>
@@ -101,12 +115,12 @@ export default function Overview({ media }) {
             {renderItems}
           </ul>
           {renderAvailableAt && (
-            <>
-              <h2>Available At: </h2>
-              <div className="overview-available-container">
-                {renderAvailableAt}
-              </div>
-            </>
+          <>
+            <h2>Available At: </h2>
+            <div className="overview-available-container">
+              {renderAvailableAt}
+            </div>
+          </>
           )}
         </div>
       </div>
