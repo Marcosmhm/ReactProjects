5b580e9ecb8af77d70bd8e440447ab238d5c0af8
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-04 23:50:42 -0300

    add token a validation fetch

--- MovieLoop/services/api.js
+++ MovieLoop/services/api.js
@@ -7,110 +7,140 @@ const options = {
 };
 
 export async function getTrending(type, randomNumber) {
-  const url = `https://api.themoviedb.org/3/trending/${type}/day?language=en-US`
-  const res = await fetch(url, options)
-  if (!res.ok) {
-    throw {
+  try {
+    const url = `https://api.themoviedb.org/3/trending/${type}/day?language=en-US`
+    const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
         message: "Failed to fetch media",
         statusText: res.statusText,
         status: res.status
       }
+    }
+    const data = await res.json()
+    const mediaAmount = randomNumber >= 0 ? data.results[randomNumber] : data.results
+    return mediaAmount
+  } catch (e) {
+    console.log(e)
   }
-  const data = await res.json()
-  const mediaAmount = randomNumber >= 0 ?  data.results[randomNumber] :  data.results
-  return mediaAmount
 }
 
 export async function getDetails(type, randomNumber) {
-  const id = (await getTrending(type, randomNumber)).id
-  const url = type === 'movie' ? `https://api.themoviedb.org/3/movie/${id}?append_to_response=videos&?language=en-US` : `https://api.themoviedb.org/3/tv/${id}?append_to_response=videos&?language=en-US` 
-  const res = await fetch(url, options)
-  if (!res.ok) {
-    throw {
+  try {
+    const id = (await getTrending(type, randomNumber)).id
+    const url = type === 'movie' ? `https://api.themoviedb.org/3/movie/${id}?append_to_response=videos&?language=en-US` : `https://api.themoviedb.org/3/tv/${id}?append_to_response=videos&?language=en-US`
+    const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
         message: "Failed to fetch details",
         statusText: res.statusText,
         status: res.status
       }
+    }
+    const data = await res.json()
+    return data
+  } catch (e) {
+    console.log(e)
   }
-  const data = await res.json()
-  return data
 }
 
 export async function getMovies(query, page = 1) {
-  let url
-  query ==='trending' ? url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&page=${page}` :
-  url = `https://api.themoviedb.org/3/movie/${query}?&page=${page}`
-  const res = await fetch(url, options)
-  if (!res.ok) {
-    throw {
+  try {
+    let url
+    query === 'trending' ? url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&page=${page}` :
+      url = `https://api.themoviedb.org/3/movie/${query}?&page=${page}`
+    const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
         message: "Failed to fetch movies",
         statusText: res.statusText,
         status: res.status
       }
+    }
+    const data = await res.json()
+    return data.results
+
+  } catch (e) {
+    console.log(e)
   }
-  const data = await res.json()
-  return data.results
 }
 
 export async function getSpecificMovie(id) {
-  const url = `https://api.themoviedb.org/3/movie/${id}?append_to_response=watch/providers%2Cvideos%2Ccredits%2Cimages%2Crecommendations&language=en-US&include_image_language=en,null`
-  const res = await fetch(url, options)
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch Movie",
-      statusText: res.statusText,
-      status: res.status
+  try {
+    const url = `https://api.themoviedb.org/3/movie/${id}?append_to_response=watch/providers%2Cvideos%2Ccredits%2Cimages%2Crecommendations&language=en-US&include_image_language=en,null`
+    const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
+        message: "Failed to fetch Movie",
+        statusText: res.statusText,
+        status: res.status
+      }
     }
+    const data = await res.json()
+    console.log(data)
+    return data
+  } catch (e) {
+    console.log(e)
   }
-  const data = await res.json()
-  console.log(data)
-  return data
 }
 
 export async function getShows(query, page = 1) {
-  let url
-  query ==='trending' ? url = `https://api.themoviedb.org/3/trending/tv/day?language=en-US&page=${page}` :
-  url = `https://api.themoviedb.org/3/tv/${query}?&page=${page}`
-  const res = await fetch(url, options)
-  if (!res.ok) {
-    throw {
+  try {
+    let url
+    query === 'trending' ? url = `https://api.themoviedb.org/3/trending/tv/day?language=en-US&page=${page}` :
+      url = `https://api.themoviedb.org/3/tv/${query}?&page=${page}`
+    const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
         message: "Failed to fetch shows",
         statusText: res.statusText,
         status: res.status
       }
+    }
+    const data = await res.json()
+    return data.results
+
+  } catch (e) {
+    console.log(e)
   }
-  const data = await res.json()
-  return data.results
 }
 
 export async function getSpecifiShow(id) {
-
-  const url = `https://api.themoviedb.org/3/tv/${id}?append_to_response=watch/providers%2Cvideos%2Ccredits%2Cimages%2Crecommendations&language=en-US&include_image_language=en,null`
-  const res = await fetch(url, options)
-  if (!res.ok) {
-    throw {
+  try {
+    const url = `https://api.themoviedb.org/3/tv/${id}?append_to_response=watch/providers%2Cvideos%2Ccredits%2Cimages%2Crecommendations&language=en-US&include_image_language=en,null`
+    const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
         message: "Failed to fetch Show",
         statusText: res.statusText,
         status: res.status
       }
+    }
+    const data = await res.json()
+    console.log(data)
+    return data
+  } catch (e) {
+    console.log(e)
   }
-  const data = await res.json()
-  console.log(data)
-  return data
+
 }
 
 export async function getPersonDetail(id) {
-  const url = `https://api.themoviedb.org/3/person/${id}?append_to_response=combined_credits%2Cexternal_ids%2Cimages&language=en-US`
-  const res = await fetch(url, options)
-  if (!res.ok) {
-    throw {
-        message: "Failed to fetch Show",
+  try {
+    const url = `https://api.themoviedb.org/3/person/${id}?append_to_response=combined_credits%2Cexternal_ids%2Cimages&language=en-US`
+    const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
+        message: "Failed to fetch Person",
         statusText: res.statusText,
         status: res.status
       }
+    }
+    const data = await res.json()
+    return data
+  } catch (e) {
+    console.log(e)
   }
-  const data = await res.json()
-  return data
 }
 
 export async function getShowSeason(id) {
@@ -121,12 +151,19 @@ export async function getShowSeason(id) {
     for (let season = 1; season <= seasonCount; season++) {
       const url = `https://api.themoviedb.org/3/tv/${id}/season/${season}?language=en-US`;
       const res = await fetch(url, options);
+      if (!res.ok) {
+        throw {
+          message: "Failed to fetch season",
+          statusText: res.statusText,
+          status: res.status
+        }
+      }
       const data = await res.json();
       allSeasonData.push(data)
     }
-  } catch(e) {
+  } catch (e) {
     console.log(e)
-  } 
+  }
   return allSeasonData
 }
 
@@ -134,11 +171,62 @@ export async function searchMulti(query) {
   try {
     const url = `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=false&language=en-US&page=1`
     const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
+        message: "Failed to fetch search",
+        statusText: res.statusText,
+        status: res.status
+      }
+    }
     const data = await res.json()
     return data.results
-  } catch (error) {
+  } catch (e) {
     console.log(e)
   }
 }
 
+export async function getToken() {
+  try {
+    const url = `https://api.themoviedb.org/3/authentication/token/new`
+    const res = await fetch(url, options)
+    if (!res.ok) {
+      throw {
+        message: "Failed to fetch token",
+        statusText: res.statusText,
+        status: res.status
+      }
+    }
+    const data = await res.jsoo()
+    return data.request_token
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export async function createSession(token) {
+  const createSessionOptions = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: `${import.meta.env.VITE_TMDB_KEY}`
+    },
+    body: JSON.stringify({request_token: token})
+  };
+  try {
+    const url = `https://api.themoviedb.org/3/authentication/session/new`
+    const res = await fetch(url, validateTokenOptions)
+    if(!res.ok) {
+      throw {
+        message: "Failed to validate token",
+        statusText: res.statusText,
+        status: res.status
+      }
+    }  
+    const data = await res.json()
+    return data.session_id
+  } catch (e) {
+    console.log(e)  
+  }
+}
 
