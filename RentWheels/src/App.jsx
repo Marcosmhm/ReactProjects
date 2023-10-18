f012c05d627a87e7e8e0913c6a34156e5cda1fa6
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-18 05:55:13 -0300

    add firebase sing up

--- RentWheels/src/App.jsx
+++ RentWheels/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import Layout from "./components/Layout"
+import Login from './pages/Login'
+import SingUp from "./pages/Singup"
 
 const router = createBrowserRouter(createRoutesFromElements(
   <>
     <Route path="/" element={<Layout />}/>
-    <Route path="/login" element="Movies goes here" />
-    <Route path="/singup" element="Specific movie goes here" />
+    <Route path="/login" element={<Login />} />
+    <Route path="/singup" element={<SingUp />} />
     <Route path="*" element="404" />
   </>
 ))
--- RentWheels/src/pages/Singup.jsx
+++ RentWheels/src/pages/Singup.jsx
@@ -1,10 +1,43 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../firebase.js'
+import { useState } from "react";
 
 function SingUp() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential)
+        const user = userCredential.user
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  }
+
   return ( 
     <>
-
+      <div className="sign-up-container">
+      <form onSubmit={handleSubmit}>
+        <h1>Create Account</h1>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
     </> 
   );
 }
