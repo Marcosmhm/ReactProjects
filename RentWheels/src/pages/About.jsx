8bc3ef215590bb41b909a9733cf99a883c7f65d7
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-18 03:52:44 -0300

    Update About.jsx

--- RentWheels/src/pages/About.jsx
+++ RentWheels/src/pages/About.jsx
@@ -1,30 +1,48 @@
 function About() {
-  return ( 
+  return (
     <>
       <section>
         <div className="container">
-          <h1 className="title orange-text">
-            ABOUT US
-          </h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus beatae eius assumenda quisquam consequuntur iure quaerat maiores similique sit delectus ad aspernatur, aut, quasi aliquam quibusdam nesciunt, odit ullam ipsam!</p>
+          <h1 className="title orange-text">ABOUT US</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
+            beatae eius assumenda quisquam consequuntur iure quaerat maiores
+            similique sit delectus ad aspernatur, aut, quasi aliquam quibusdam
+            nesciunt, odit ullam ipsam!
+          </p>
           <div className="our-container">
             <div className="our-story-container">
-              <h1 className="orange-text">OUR STORY</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus itaque ab magnam facilis excepturi earum impedit delectus eum eligendi! A enim doloribus consequatur delectus iste inventore dicta, quas consequuntur officia!</p>
+              <h3 className="orange-text">OUR STORY</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repellendus itaque ab magnam facilis excepturi earum impedit
+                delectus eum eligendi! A enim doloribus consequatur delectus
+                iste inventore dicta, quas consequuntur officia!
+              </p>
             </div>
             <div className="our-approach-container">
-              <h1 className="orange-text">OUR APPROACH</h1>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, necessitatibus provident voluptatem, quaerat dicta quibusdam tenetur blanditiis velit voluptates magnam beatae, ipsa molestias dolorum maiores cupiditate ea facilis harum rem.</p>
+              <h3 className="orange-text">OUR APPROACH</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Asperiores, necessitatibus provident voluptatem, quaerat dicta
+                quibusdam tenetur blanditiis velit voluptates magnam beatae,
+                ipsa molestias dolorum maiores cupiditate ea facilis harum rem.
+              </p>
             </div>
             <div className="our-mission-container">
-              <h1 className="orange-text">OUR MISSION</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea consequatur beatae! Nihil voluptas tempora asperiores non consequatur reiciendis ipsam minima adipisci deleniti nesciunt ullam, placeat consectetur vitae eligendi soluta.</p>
+              <h3 className="orange-text">OUR MISSION</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
+                ea consequatur beatae! Nihil voluptas tempora asperiores non
+                consequatur reiciendis ipsam minima adipisci deleniti nesciunt
+                ullam, placeat consectetur vitae eligendi soluta.
+              </p>
             </div>
           </div>
         </div>
       </section>
     </>
-   );
+  );
 }
 
 export default About;
