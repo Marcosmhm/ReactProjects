12fe7823dcd6dc9f8b10b5bc38caa7367c0cd190
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-18 00:40:44 -0300

    add vehicles data

--- RentWheels/src/pages/Inventory.jsx
+++ RentWheels/src/pages/Inventory.jsx
@@ -1,9 +1,13 @@
 function Inventory() {
-  return ( 
+  return (
     <>
+      <div className="inventory-btn-container">
+        <button>Manual</button>
+        <button>Automatic</button>
+      </div>
       <h1>Latest Inventory</h1>
     </>
-   );
+  );
 }
 
 export default Inventory;
