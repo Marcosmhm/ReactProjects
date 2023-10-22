a6c384fa7be946d867f023103ba420cc9b22781c
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-22 04:04:56 -0300

    Update BookForm.jsx

--- RentWheels/src/components/BookForm.jsx
+++ RentWheels/src/components/BookForm.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 function BookForm() {
+  const [formData, setFormData] = useState({
+    carType: "",
+    pickUpLocation: "",
+    dropOf: "",
+    pickUpDate: "",
+    dropOfDate: "",
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name] : value
+      }
+    })
+  }
   return ( 
      <>
-      
+      <form action="">
+        <label htmlFor="">Select Your Car Type</label>
+        <select name="carType" id="" value={formData.carType}>
+          <option value="">test</option>
+          <option value="">test</option>
+          <option value="">test</option>
+        </select>
+      </form>
      </>
    );
 }
