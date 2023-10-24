a025f19ad7bcbd40ecc29db0de4b3f7864218011
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-23 23:52:20 -0300

    add styling for inventory vehicles

--- RentWheels/src/components/CarCard.jsx
+++ RentWheels/src/components/CarCard.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
 import vehicles from "../data/vehicles.js";
+
 function CarCard({ filter }) {
+  const [vehicleData, setVehicleData] = useState([{}]);
+
+  const handleVehicleClick = (id) => {
+    const newData = vehicles.filter((vehicle) => vehicle.id === id)[0];
+    console.log(newData);
+    setVehicleData(newData);
+    console.log("oi", vehicleData);
+  };
+
   const renderVehicles = vehicles
     .filter((vehicle) => {
       return filter === "all"
@@ -8,22 +19,57 @@ function CarCard({ filter }) {
     })
     .map((vehicle, index) => {
       return (
-        <div className="car-card" key={`${vehicle.make}-${index}`}>
-          <div className="image-container">
-            <span>
-
-            <img src={vehicle.image_url.toString()} alt="" />
-            </span>
-          </div>
-          <h3>
-            {vehicle.year} {vehicle.make} {vehicle.model}
-          </h3>
-          <button className="car-card-btn">BOOK NOW</button>
+        <div className="car-div" onClick={() => handleVehicleClick(vehicle.id)}>
+          {vehicle.name}
         </div>
       );
     });
 
-  return <>{renderVehicles}</>;
+  const renderVehicleInfo = (
+    <>
+      <div className="selected-vehicle">
+        <div className="image-container">
+          <img src={vehicleData.image_url} alt="" />
+        </div>
+        <div className="selected-vehicle-price">
+          ${vehicleData.price_per_day} / Per day
+        </div>
+        <ul>
+          <li>
+            <span className="list-title">Model</span>
+            <span className="list-content">{vehicleData.model}</span>
+          </li>
+          <li>
+            <span className="list-title">Make</span>
+            <span className="list-content">{vehicleData.make}</span>
+          </li>
+          <li>
+            <span className="list-title">Year</span>
+            <span className="list-content">{vehicleData.year}</span>
+          </li>
+          <li>
+            <span className="list-title">Seats</span>
+            <span className="list-content">{vehicleData.seats}</span>
+          </li>
+          <li>
+            <span className="list-title">Transmission</span>
+            <span className="list-content">{vehicleData.transmission}</span>
+          </li>
+          <li>
+            <span className="list-title">Type</span>
+            <span className="list-content">{vehicleData.type}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      {renderVehicles}
+      {renderVehicleInfo}
+    </>
+  );
 }
 
 export default CarCard;
