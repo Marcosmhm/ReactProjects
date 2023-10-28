13b3adfd630b9f8eed38377c2eff4b2c07ddbfee
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-27 21:30:12 -0300

    Update Home.jsx

--- RentWheels/src/pages/Home.jsx
+++ RentWheels/src/pages/Home.jsx
@@ -9,7 +9,9 @@ function Home() {
     <>
       <section id="home" className="home-section">
         <div className="container">
+          <img src="/src/assets/images/home-bg2.png" alt="" className="bg-shape" />
           <div className="home-content">
+            <h4>Plan your trip now</h4>
             <h1>
               <span className="orange-text">EASY</span> AND
               <span className="orange-text"> FAST</span> WAY TO RENT YOUR CAR
@@ -19,10 +21,11 @@ function Home() {
               flexible pick-up options and much more.
             </p>
             <div className="home-btn-container">
-              <button onClick={() => handleClickScroll('book-car')} className="home-btn book-car">GET STARTED</button>
+              <button onClick={() => handleClickScroll('book-car')} className="home-btn book-car">Get Started</button>
             </div>
           </div>
         </div>
+        <img src="/src/assets/images/red-car.png" alt="" className="home-img" />
       </section>
       <BookForm />
     </>
