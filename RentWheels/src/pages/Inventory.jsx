3f6dec3942859ff31c1bf7f809573a1dfeeaeebb
Marcos Henrique <mh.moraes@hotmail.com>
2023-10-28 22:44:50 -0300

    Update Inventory.jsx

--- RentWheels/src/pages/Inventory.jsx
+++ RentWheels/src/pages/Inventory.jsx
@@ -12,9 +12,11 @@ function Inventory() {
     <>
       <section className="inventory-section" id="fleet"> 
         <div className="container">
-        <h1 className="title">
-          <span className="orange-text">LATEST</span> INVENTORY
-        </h1>
+        <div className="title">
+          <h3>Vehicle Models</h3>
+          <h1>Our rental fleet</h1>
+          <p>Choose from a variety of our amazing vehicles to rent for your next adventure or business trip</p>
+        </div>
           <div className="inventory-btn-container">
             <button className="inventory-btn" onClick={() => handleVehicleTypeClick("all")}>All</button>
             <button className="inventory-btn" onClick={() => handleVehicleTypeClick("manual")}>
