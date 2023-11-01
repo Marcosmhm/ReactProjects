3826e5e602980485927150013caaf9a2cbc0dbc5
Marcos Henrique <mh.moraes@hotmail.com>
2023-11-01 00:59:11 -0300

    add testimonials component to layout

--- RentWheels/src/components/Layout.jsx
+++ RentWheels/src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import About from "../pages/About";
 import Home from "../pages/Home";
 import Inventory from "../pages/Inventory";
+import Testimonials from "../pages/Testimonials";
 import WhyUs from "../pages/WhyUs";
 import Navbar from "./Navbar";
 
@@ -11,6 +12,7 @@ function Layout() {
       <Home />
       <WhyUs />
       <Inventory />
+      <Testimonials />
       <About />
     </>
    );
