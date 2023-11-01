16d06d6de98537ffb485bf1cfd3fdf3304caca65
Marcos Henrique <mh.moraes@hotmail.com>
2023-11-01 00:59:37 -0300

    add title container in testimonials

--- RentWheels/src/pages/Testimonials.jsx
+++ RentWheels/src/pages/Testimonials.jsx
@@ -1,7 +1,15 @@
 function Testimonials() {
   return ( 
     <>
-      
+      <section>
+        <div className="container">
+          <div className="title">
+            <h3>Review by People</h3>
+            <h1>Client's Testimonials</h1>
+            <p>Discover the positive impact we've made on the our clients by reading through their testimonials. Our clients have experienced our service and results, and they're eager to share their positive experiences with you.</p>
+          </div>
+        </div>
+      </section>
     </>
    );
 }
