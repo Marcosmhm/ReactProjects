fa23a43a6f8c3d0690907a67e983bae642ea2508
Marcos Henrique <mh.moraes@hotmail.com>
2023-11-19 12:53:18 -0200

    add board square click function
    funcionalities added:
     change turn;
     change player mark;
     checking if the square is currently held by a player

--- TicTacToe/src/components/Board.tsx
+++ TicTacToe/src/components/Board.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 
 function Board () {
-  
+  const [currentPlayer, setCurrentPlayer] = useState('player1')
+  const [currentPlayerMark, setCurrentPlayerMark] = useState('X')
+
+  //board square types
   type BoardElementsTypes = {
     id: number
+    isHeld: boolean
     heldBy: string
     playerMark: string
   }[]
   
+  //creating the objects that will be sent to the board
   const createBoardElements = () => {
     const boardElementsArray:BoardElementsTypes = []
     for (let i = 0; i < 9; i++) {
       boardElementsArray.push({
         id: i,
+        isHeld: false,
         heldBy:'', 
         playerMark: '',
       })
@@ -20,13 +26,36 @@ function Board () {
     return boardElementsArray
   }
 
+  //array of objects containing the squares info
   const [boardElements, setBoardElements] = useState(createBoardElements())
 
+  //changes current playermark and name
+  const changePlayerTurn = () => {
+    setCurrentPlayer(currentPlayer === 'player1' ? 'player2' : 'player1')
+    setCurrentPlayerMark(currentPlayer === 'player1' ? 'O' : 'X')
+  }
+
+  const handleBoardSquareClick = (id: number) => {
+    const clickedEl = boardElements.filter(element => element.id === id)[0]
+
+    //checks if the clicked square is currently being held
+    if (!clickedEl.isHeld) {
+      setBoardElements(prevState => prevState.map(element => {
+        return element.id === id ? {...element, isHeld: true,playerMark: currentPlayerMark, heldBy: currentPlayer} :
+        element
+      }))
+      changePlayerTurn()
+    }
+  }
+
+  //function to render the board squares to the screen
   const renderBoardElements = () => {
     return boardElements.map(element => {
       return (
-        <div className="board-square">
-          
+        <div className="board-square" onClick={() => handleBoardSquareClick(element.id)} >
+          <div className="board-square-content">
+            {element.playerMark}
+          </div>
         </div>
       )
     })
