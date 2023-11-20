929762ccc506c32daf47344d10a74108416f45a4
Marcos Henrique <mh.moraes@hotmail.com>
2023-11-20 15:41:59 -0200

    code formating

--- RentWheels/src/pages/Inventory.jsx
+++ RentWheels/src/pages/Inventory.jsx
@@ -10,19 +10,33 @@ function Inventory() {
 
   return (
     <>
-      <section className="inventory-section" id="fleet"> 
+      <section className="inventory-section" id="fleet">
         <div className="container">
-        <div className="title">
-          <h3>Vehicle Models</h3>
-          <h1>Our rental fleet</h1>
-          <p>Choose from a variety of our amazing vehicles to rent for your next adventure or business trip</p>
-        </div>
+          <div className="title">
+            <h3>Vehicle Models</h3>
+            <h1>Our rental fleet</h1>
+            <p>
+              Choose from a variety of our amazing vehicles to rent for your
+              next adventure or business trip
+            </p>
+          </div>
           <div className="inventory-btn-container">
-            <button className="inventory-btn" onClick={() => handleVehicleTypeClick("all")}>All</button>
-            <button className="inventory-btn" onClick={() => handleVehicleTypeClick("manual")}>
+            <button
+              className="inventory-btn"
+              onClick={() => handleVehicleTypeClick("all")}
+            >
+              All
+            </button>
+            <button
+              className="inventory-btn"
+              onClick={() => handleVehicleTypeClick("manual")}
+            >
               Manual
             </button>
-            <button className="inventory-btn" onClick={() => handleVehicleTypeClick("automatic")}>
+            <button
+              className="inventory-btn"
+              onClick={() => handleVehicleTypeClick("automatic")}
+            >
               Automatic
             </button>
           </div>
