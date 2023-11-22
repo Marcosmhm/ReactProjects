07a163b96d837a83cc15e279d49d644fa4702c83
Marcos Henrique <mh.moraes@hotmail.com>
2023-11-22 20:13:42 -0200

    finish download section styling

--- RentWheels/src/components/Download.jsx
+++ RentWheels/src/components/Download.jsx
@@ -1,13 +1,22 @@
 function Download() {
-  return ( 
+  return (
     <>
-      <section>
+      <section className="download-section">
         <div className="container">
-          
+          <h1>Download our app to get most out of it</h1>
+          <p>
+            Thrown shy denote ten ladies though ask saw. Or by to he going think
+            order event music. Incommode so intention defective at convinced.
+            Led income months itself and houses you.
+          </p>
+          <div className="download-imgs">
+            <img src="src/assets/images/google-app-download-1.svg" alt="" />
+            <img src="src/assets/images/ios-app-btn.svg" alt="" />
+          </div>
         </div>
       </section>
     </>
-   );
+  );
 }
 
 export default Download;
