c41285876abc8a14d2aaa38eab223f2ab8a15149
Marcos Henrique <mh.moraes@hotmail.com>
2023-11-22 19:30:14 -0200

    add span classes for footer

--- RentWheels/src/components/Footer.jsx
+++ RentWheels/src/components/Footer.jsx
@@ -1,15 +1,20 @@
 function Footer() {
-  return ( 
+  return (
     <>
       <footer>
         <div className="container">
           <div className="footer-flex-wrapper">
             <ul>
               <li className="footer-list-title">Rent Wheels</li>
-              <li>We provide a wide range of vehicles for all your driving needs. We have the perfect car to meet your needs.</li>
+              <li>
+                {" "}
+                <span>
+                  We provide a wide range of vehicles for all your driving
+                  needs. We have the perfect car to meet your needs.
+                </span>
+              </li>
               <li>(123)-456-789</li>
               <li>RentWheels@gmail.com</li>
-              <li>Design by XpeedStudio</li>
             </ul>
             <ul>
               <li className="footer-list-title">Company</li>
@@ -21,27 +26,31 @@ function Footer() {
             </ul>
             <ul>
               <li className="footer-list-title">Working Hours</li>
-              <li>Mon - Fri: 9:00AM - 9:00PM</li>
-              <li>Sat: 9:00AM - 19:00PM</li>
-              <li>Sun: Closed</li>
+              <li>
+                <span>Mon - Fri:</span> 9:00AM - 9:00PM
+              </li>
+              <li>
+                <span>Sat:</span> 9:00AM - 19:00PM
+              </li>
+              <li>
+                <span>Sun:</span> Closed
+              </li>
             </ul>
             <ul>
               <li className="footer-list-title">Subscription</li>
-              <li>Subscribe your Email address for latest news & updates</li>
+              <li><span>Subscribe your Email address for latest news & updates</span></li>
               <li>
-                <input type="email" name="email" id="" />
+                <input type="email" name="email" id="" placeholder="Enter Email Address"/>
               </li>
               <li>
-                <button type="submit">
-                  Submit
-                </button>
+                <button type="submit">Submit</button>
               </li>
             </ul>
           </div>
         </div>
       </footer>
     </>
-   );
+  );
 }
 
 export default Footer;
