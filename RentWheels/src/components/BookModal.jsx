9a65795702fc7084e4de5dfe7de9879e5010a22e
Marcos Henrique <mh.moraes@hotmail.com>
2023-11-24 18:34:29 -0200

    update booking modal form

--- RentWheels/src/components/BookModal.jsx
+++ RentWheels/src/components/BookModal.jsx
@@ -7,63 +7,74 @@ function BookModal({ ...props }) {
     <>
       <section className="book-modal">
         <div className="book-modal-wrapper">
-          <h1>COMPLETE RESERVATION</h1>
-          <div className="book-modal-content">
-            <div className="book-info">
-              <h3>
-                <FaCircleInfo size={24} /> Upon completing this reservation
-                enquiry, you will receive:
-              </h3>
-              <p>
-                Your rental voucher to produce on arrival at the rental desk and
-                a toll-free customer support number
-              </p>
-            </div>
-            <div className="booking-data">
-              <div className="booking-left">
-                <h3>Location & Date</h3>
-                <div className="pickup-info">
-                  <IoLocationOutline size={20} />
-                  <div className="pickup-info-content">
-                    <span>Pick-Up Date</span>
-                    {props.pickUpDate}
+          <div className="book-modal-container">
+            <h1>COMPLETE RESERVATION</h1>
+            <div className="book-modal-content">
+              <div className="book-info">
+                <h3>
+                  <FaCircleInfo /> Upon completing this reservation enquiry, you
+                  will receive:
+                </h3>
+                <p>
+                  Your rental voucher to produce on arrival at the rental desk
+                  and a toll-free customer support number
+                </p>
+              </div>
+              <div className="booking-data">
+                <div className="booking-left">
+                  <h3>Location & Date</h3>
+                  <div className="pickup-info">
+                    <IoLocationOutline size={20} />
+                    <div className="pickup-info-content">
+                      <span>Pick-Up Date</span>
+                      {props.pickUpDate.replaceAll("-", "/")}
+                    </div>
+                  </div>
+                  <div className="pickup-info">
+                    <IoLocationOutline size={20} />
+                    <div className="pickup-info-content">
+                      <span>Drop-Off Date</span>
+                      {props.dropOfDate.replaceAll("-", "/")}
+                    </div>
+                  </div>
+                  <div className="pickup-info">
+                    <MdOutlineEditCalendar size={20} />
+                    <div className="pickup-info-content">
+                      <span>Pick-Up Location</span>
+                      {props.pickUpLocation}
+                    </div>
+                  </div>
+                  <div className="pickup-info">
+                    <MdOutlineEditCalendar size={20} />
+                    <div className="pickup-info-content">
+                      <span>Drop-Off Location</span>
+                      {props.dropOf}
+                    </div>
                   </div>
                 </div>
-                <div className="pickup-info">
-                  <IoLocationOutline size={20} />
-                  <div className="pickup-info-content">
-                    <span>Drop-Off Date</span>
-                    {props.dropOfDate}
-                  </div>
-                </div>
-                <div className="pickup-info">
-                  <MdOutlineEditCalendar size={20} />
-                  <div className="pickup-info-content">
-                    <span>Pick-Up Location</span>
-                    {props.pickUpLocation}
-                  </div>
-                </div>
-                <div className="pickup-info">
-                  <MdOutlineEditCalendar size={20} />
-                  <div className="pickup-info-content">
-                    <span>Drop-Off Location</span>
-                    {props.dropOf}
-                  </div>
+                <div className="booking-right">
+                  <h3>Car: {props.carModel}</h3>
                 </div>
               </div>
-              <div className="booking-right">
-                <h3>Car: {props.carModel}</h3>
+              <div className="booking-personal-form">
+                <h3>PERSONAL INFORMATION</h3>
+                <label htmlFor="first-name">First Name</label>
+                <input type="text" id="first-name" name="first-name" placeholder="Enter your first name" />
+                <label htmlFor="last-name">Last Name</label>
+                <input type="text" name="last-name" id="last-name" placeholder="Enter your last name" />
+                <label htmlFor="">Phone Number</label>
+                <input type="text" placeholder="Enter your phone number" />
+                <label htmlFor="">Age</label>
+                <input type="text" placeholder="18" />
+                <label htmlFor="">Email</label>
+                <input type="text" placeholder="Enter your email address" />
+                <label htmlFor="">Address</label>
+                <input type="text" placeholder="Enter your street address" />
+                <label htmlFor="">City</label>
+                <input type="text" placeholder="Enter your city" />
+                <label htmlFor="">Zip code</label>
+                <input type="text" placeholder="Enter your zip code" />
               </div>
-            </div>
-            <div className="booking-personal-form">
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
             </div>
           </div>
         </div>
